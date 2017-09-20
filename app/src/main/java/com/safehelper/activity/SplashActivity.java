e69470e25b5130dd623d0b2bbf989591df6b6ba3
lt1023 <1023968391@qq.com>
2017-09-20 11:40:09 +0800

    更改

--- app/src/main/java/com/safehelper/activity/SplashActivity.java
+++ app/src/main/java/com/safehelper/activity/SplashActivity.java
@@ -1,20 +1,42 @@
 package com.safehelper.activity;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.safehelper.R;
-
+import com.safehelper.utils.ToastUtil;
 import static com.safehelper.utils.NetWorkUtil.isNetworkAvailable;
 
-public class SplashActivity extends AppCompatActivity {
 
+public class SplashActivity extends AppCompatActivity {
+    private static final int SPLASH_TO_MAIN = 0X123;
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == SPLASH_TO_MAIN){
+                checkNetWork();
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
-        isNetworkAvailable(this);
-        //
+        mHandler.sendEmptyMessageDelayed(SPLASH_TO_MAIN,5*1000);
     }
 
+    private void checkNetWork() {
+        if (isNetworkAvailable(this)){
+            turn2MainActivity();
+            finish();
+        }else {
+            ToastUtil.doToast(this,"网络连接失败");
+        }
+    }
+
+    private void turn2MainActivity() {
+        startActivity(new Intent(this,MainActivity.class));
+    }
 }
