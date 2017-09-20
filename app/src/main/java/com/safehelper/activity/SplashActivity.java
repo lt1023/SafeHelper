2b70146c27a8ec27f284f644dd63361e20494e2b
lt1023 <1023968391@qq.com>
2017-09-20 10:56:45 +0800

    更改

--- app/src/main/java/com/safehelper/activity/SplashActivity.java
+++ app/src/main/java/com/safehelper/activity/SplashActivity.java
@@ -1,7 +1,10 @@
-package com.safehelper;
+package com.safehelper.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.safehelper.R;
+
+import static com.safehelper.utils.NetWorkUtil.isNetworkAvailable;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +13,7 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        isNetworkAvailable(this);
     }
+
 }
