757c8729ffa4061474098e6e580fc84f48b53e00
lt1023 <1023968391@qq.com>
2017-09-21 16:07:09 +0800

    检测版本更新

--- app/src/main/java/com/safehelper/activity/MainActivity.java
+++ app/src/main/java/com/safehelper/activity/MainActivity.java
@@ -1,6 +1,8 @@
 package com.safehelper.activity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -14,6 +16,9 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
 import static com.safehelper.utils.UpdateUtil.getPackageVersion;
 import static com.safehelper.utils.UpdateUtil.stream2String;
 
@@ -31,14 +36,22 @@ public class MainActivity extends Activity{
     private static final int UPDATEVERSION = 0x124;
     //获取包的版本
     private float PACKAGEVERSION;
-
+    //最新版本
+    String versionName;
+    //新版本介绍
+    private String versionDesc;
+    //新版本下载地址
+    private String downloadUrl;
     private Handler mHandler = new Handler(){
         @Override
         public void handleMessage(Message msg) {
             switch (msg.what){
                 case NEWESTVERSION:
-                    String versionName = msg.obj.toString();
-                    ToastUtil.doToast(MainActivity.this,"最新版本是：" + versionName + "请更新");
+                    Bundle data = msg.getData();
+                    versionName = data.getString("versionName");
+                    versionDesc = data.getString("versionDesc");
+                    downloadUrl = data.getString("downloadUrl");
+                    showUpdateDialog(versionName,versionDesc,downloadUrl);
                 break;
                 case UPDATEVERSION:
                     ToastUtil.doToast(MainActivity.this,"恭喜你是最新的版本");
@@ -48,6 +61,26 @@ public class MainActivity extends Activity{
             }
         }
     };
+
+    private void showUpdateDialog(String name, String desc,final String url) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(name + "版本更新");
+        builder.setMessage(desc);
+        builder.setPositiveButton("下次再说", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.setNegativeButton("立即更新", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                ToastUtil.doToast(MainActivity.this,"更新地址是：" + url);
+            }
+        });
+        builder.show();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -68,6 +101,7 @@ public class MainActivity extends Activity{
             @Override
             public void run() {
                 Message message ;
+                Bundle data = new Bundle();
                 try {
                     URL updateUrl = new URL(update_url);
                     HttpURLConnection connection = (HttpURLConnection) updateUrl.openConnection();
@@ -80,11 +114,15 @@ public class MainActivity extends Activity{
                     final String versionName = updatejson.getString("versionName");
                     final String versionDesc = updatejson.getString("versionDesc");
                     final String downloadUrl = updatejson.getString("downloadUrl");
+                    data.putString("versionCode",versionCode);
+                    data.putString("versionName",versionName);
+                    data.putString("versionDesc",versionDesc);
+                    data.putString("downloadUrl",downloadUrl);
                     if (currentVersion <Float.parseFloat(versionCode)){
                         Log.i(TAG,"最新版本为:" + versionName);
                         message = new Message();
                         message.what = NEWESTVERSION;
-                        message.obj = versionName;
+                        message.setData(data);
                         mHandler.sendMessage(message);
                     }else {
                         message = new Message();
